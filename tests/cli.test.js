c2513ca29fb2e60ecf4f90f6870ab58f43cdc5a7
maxlath <k@maxlath.eu>
2025-10-24 00:17:23 +0200

    fix json valueEncoding support

--- tests/cli.test.js
+++ tests/cli.test.js
@@ -14,9 +14,60 @@ describe('cli', () => {
   describe('put', () => {
     it('should put a value', async () => {
       const value = getRandomString()
-      await shellExecLev(`--put foo --value ${value}`)
-      const { stdout } = await shellExecLev('--get foo')
-      stdout.should.equal(value)
+      const { stdout, stderr } = await shellExecLev(`--put foo --value ${value}`)
+      stdout.should.equal('')
+      stderr.should.equal('')
+      const { stdout: getStdout } = await shellExecLev('--get foo')
+      getStdout.should.equal(value)
+    })
+  })
+
+  describe('valueEncoding', () => {
+    describe('utf8 (default)', async () => {
+      const value = getRandomString()
+      before(async () => {
+        await shellExecLev(`--put foo --value ${value}`)
+      })
+      it('--get', async () => {
+        const { stdout } = await shellExecLev('--get foo')
+        stdout.should.equal(value)
+      })
+      it('--all/--prefix', async () => {
+        const { stdout: stdout } = await shellExecLev('--prefix foo')
+        const lines = stdout.trim().split('\n')
+        lines.length.should.equal(1)
+        lines[0].should.equal(`{"key":"foo","value":"${value}"}`)
+      })
+      it('--values', async () => {
+        const { stdout } = await shellExecLev('--values foo')
+        const lines = stdout.trim().split('\n')
+        lines.length.should.equal(1)
+        lines[0].should.equal(value)
+      })
+    })
+
+    describe('json', async () => {
+      const value = getRandomString()
+      const json = `{"a":"${value}"}`
+      before(async () => {
+        await shellExecLev(`--put foo --value '${json}' --valueEncoding json`)
+      })
+      it('--get', async () => {
+        const { stdout } = await shellExecLev('--get foo --valueEncoding json')
+        stdout.should.equal(json)
+      })
+      it('--all/--prefix', async () => {
+        const { stdout: prefixStdout } = await shellExecLev('--prefix foo --valueEncoding json')
+        const lines = prefixStdout.trim().split('\n')
+        lines.length.should.equal(1)
+        lines[0].should.equal(`{"key":"foo","value":${json}}`)
+      })
+      it('--values', async () => {
+        const { stdout: prefixStdout } = await shellExecLev('--values foo --valueEncoding json')
+        const lines = prefixStdout.trim().split('\n')
+        lines.length.should.equal(1)
+        lines[0].should.equal(json)
+      })
     })
   })
 })
